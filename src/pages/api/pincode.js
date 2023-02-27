d7ea9de67e30b00946d6629223ad32527eeca2f2
“Akriti <akritiyadav895@gmail.com>
2023-02-27 16:48:21 +0530

    next-js

---
+++ src/pages/api/pincode.js
@@ -0,0 +1,6 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+    res.status(200).json([224143,224129,273010,110003])
+  }
+  
---
+++ src/pages/hoodies.js
@@ -0,0 +1,106 @@
+import React from 'react';
+
+const Hoodies = () => {
+  return (
+    <div>
+ 
+ <section class="text-gray-600 body-font">
+  <div class="container px-5 py-24 mx-auto">
+    <div class="flex flex-wrap -m-4">
+      <div class="lg:w-1/4 md:w-1/2 p-4 w-full shadow-lg m-0 ">
+        <a class="block relative  rounded overflow-hidden">
+          <img alt="ecommerce" class=" m-auto md:mx-0 h-[30vh] md:h-[36vh] block" src="https://m.media-amazon.com/images/I/61ZNc27pJ6L._UY606_.jpg"/>
+        </a>
+        <div class="mt-4 text-center md:text-left">
+          <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">Hoodies</h3>
+          <h2 class="text-gray-900 title-font text-lg font-medium">Wear the code</h2>
+          <p class="mt-1">₹1219.00</p>
+          <p class="mt-1">S, M, L, XL,XXL</p>
+        </div>
+      </div>
+      <div class="lg:w-1/4 md:w-1/2 p-4 w-full shadow-lg m-0 ">
+        <a class="block relative  rounded overflow-hidden">
+          <img alt="ecommerce" class=" m-auto md:mx-0 h-[30vh] md:h-[36vh] block" src="https://m.media-amazon.com/images/I/61ZNc27pJ6L._UY606_.jpg"/>
+        </a>
+        <div class="mt-4 text-center md:text-left">
+          <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">Hoodies</h3>
+          <h2 class="text-gray-900 title-font text-lg font-medium">Wear the code</h2>
+          <p class="mt-1">₹1219.00</p>
+          <p class="mt-1">S, M, L, XL,XXL</p>
+        </div>
+      </div>
+      <div class="lg:w-1/4 md:w-1/2 p-4 w-full shadow-lg m-0 ">
+        <a class="block relative  rounded overflow-hidden">
+          <img alt="ecommerce" class=" m-auto md:mx-0 h-[30vh] md:h-[36vh] block" src="https://m.media-amazon.com/images/I/61ZNc27pJ6L._UY606_.jpg"/>
+        </a>
+        <div class="mt-4 text-center md:text-left">
+          <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">Hoodies</h3>
+          <h2 class="text-gray-900 title-font text-lg font-medium">Wear the code</h2>
+          <p class="mt-1">₹1219.00</p>
+          <p class="mt-1">S, M, L, XL,XXL</p>
+        </div>
+      </div>
+      <div class="lg:w-1/4 md:w-1/2 p-4 w-full shadow-lg m-0 ">
+        <a class="block relative  rounded overflow-hidden">
+          <img alt="ecommerce" class=" m-auto md:mx-0 h-[30vh] md:h-[36vh] block" src="https://m.media-amazon.com/images/I/61ZNc27pJ6L._UY606_.jpg"/>
+        </a>
+        <div class="mt-4 text-center md:text-left">
+          <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">Hoodies</h3>
+          <h2 class="text-gray-900 title-font text-lg font-medium">Wear the code</h2>
+          <p class="mt-1">₹1219.00</p>
+          <p class="mt-1">S, M, L, XL,XXL</p>
+        </div>
+      </div>
+      <div class="lg:w-1/4 md:w-1/2 p-4 w-full shadow-lg m-0 ">
+        <a class="block relative  rounded overflow-hidden">
+          <img alt="ecommerce" class=" m-auto md:mx-0 h-[30vh] md:h-[36vh] block" src="https://m.media-amazon.com/images/I/61ZNc27pJ6L._UY606_.jpg"/>
+        </a>
+        <div class="mt-4 text-center md:text-left">
+          <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">Hoodies</h3>
+          <h2 class="text-gray-900 title-font text-lg font-medium">Wear the code</h2>
+          <p class="mt-1">₹1219.00</p>
+          <p class="mt-1">S, M, L, XL,XXL</p>
+        </div>
+      </div>
+      <div class="lg:w-1/4 md:w-1/2 p-4 w-full shadow-lg m-0 ">
+        <a class="block relative  rounded overflow-hidden">
+          <img alt="ecommerce" class=" m-auto md:mx-0 h-[30vh] md:h-[36vh] block" src="https://m.media-amazon.com/images/I/61ZNc27pJ6L._UY606_.jpg"/>
+        </a>
+        <div class="mt-4 text-center md:text-left">
+          <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">Hoodies</h3>
+          <h2 class="text-gray-900 title-font text-lg font-medium">Wear the code</h2>
+          <p class="mt-1">₹1219.00</p>
+          <p class="mt-1">S, M, L, XL,XXL</p>
+        </div>
+      </div>
+      <div class="lg:w-1/4 md:w-1/2 p-4 w-full shadow-lg m-0 ">
+        <a class="block relative  rounded overflow-hidden">
+          <img alt="ecommerce" class=" m-auto md:mx-0 h-[30vh] md:h-[36vh] block" src="https://m.media-amazon.com/images/I/61ZNc27pJ6L._UY606_.jpg"/>
+        </a>
+        <div class="mt-4 text-center md:text-left">
+          <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">Hoodies</h3>
+          <h2 class="text-gray-900 title-font text-lg font-medium">Wear the code</h2>
+          <p class="mt-1">₹1219.00</p>
+          <p class="mt-1">S, M, L, XL,XXL</p>
+        </div>
+      </div>
+      <div class="lg:w-1/4 md:w-1/2 p-4 w-full shadow-lg m-0 ">
+        <a class="block relative  rounded overflow-hidden">
+          <img alt="ecommerce" class=" m-auto md:mx-0 h-[30vh] md:h-[36vh] block" src="https://m.media-amazon.com/images/I/61ZNc27pJ6L._UY606_.jpg"/>
+        </a>
+        <div class="mt-4 text-center md:text-left">
+          <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">Hoodies</h3>
+          <h2 class="text-gray-900 title-font text-lg font-medium">Wear the code</h2>
+          <p class="mt-1">₹1219.00</p>
+          <p class="mt-1">S, M, L, XL,XXL</p>
+        </div>
+      </div>
+
+    </div>
+  </div>
+</section>
+    </div>
+  );
+}
+
+export default Hoodies;
